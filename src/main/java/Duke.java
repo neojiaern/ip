1e93b9861ae815f92c57b108cb84117e22ad6fe9
neojiaern <e0417959@u.nus.edu>
2020-08-22 22:18:53 +0800

    Implement add and list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 
 public class Duke {
+
+    // list the tasks
+    public static void listTasks(String[] list, int count){
+        if (count == 0){
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     There is currently no task.");
+            System.out.println("    ____________________________________________________________\n");
+        }
+        else{
+            System.out.println("    ____________________________________________________________");
+            for(int i = 1; i <= count; i++){
+                System.out.println("     " + i + ". " + list[i-1]);
+            }
+            System.out.println("    ____________________________________________________________\n");
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -18,10 +35,19 @@ public class Duke {
         String userInput;
         Scanner in = new Scanner(System.in);
         userInput = in.nextLine();
+        String[] list = new String[100];
+        int count = 0;
         while (!userInput.equalsIgnoreCase("bye")) {
-            System.out.println("    ____________________________________________________________");
-            System.out.println("      " + userInput);
-            System.out.println("    ____________________________________________________________\n");
+            if (userInput.equalsIgnoreCase("list")){
+                listTasks(list, count);
+            }
+            else {
+                list[count] = userInput;
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     added: " + userInput);
+                System.out.println("    ____________________________________________________________\n");
+                count++;
+            }
             userInput = in.nextLine();
         };
 
